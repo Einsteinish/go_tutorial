beeca629e89d4e364d8bf5d12c3cc9ce0a457528
Einsteinish <kihyuck.hong@gmail.com>
2023-11-16 11:41:52 -0800

    adding samples

---
+++ samples/go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.21.4
+
+require github.com/einsteinish/go_tutorial v0.0.0-20231115000907-59bccb90e58f // indirect
---
+++ samples/hello_world.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/einsteinish/go_tutorial/string_util"
+)
+
+func main() {
+    reversedString := reverse.String("Hello, world!")
+    fmt.Println(reversedString) // Output: !dlrow ,olleH
+}
