6cac16c28df07cb4dea8b7c1737b574578a7f421
Einsteinish <kihyuck.hong@gmail.com>
2023-11-21 16:55:58 -0800

    adding authentication

---
+++ authentication/2-BasicAuth/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/resource", gin.BasicAuth(gin.Accounts{
+		"admin": "secret",
+	}), func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": "resource data",
+		})
+	})
+	r.Run() // Listening and serving HTTP on :8080
+}
